a881bcda3a9e091751ccbe3403dc5b781abbced1
Naol Kidanu <sabelkidanu18@gmail.com>
2023-09-20 19:20:43 +0300

    fix linter error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "../style.css";
-import CalcBtns from "./CalcBtns";
+import React from 'react';
+import '../style.css';
+import CalcBtns from './CalcBtns';
 
 const Calculator = () => (
   <>
