8f6a16034a9430601eca47489a4bad0a2b38c766
Naol Kidanu <sabelkidanu18@gmail.com>
2023-09-21 22:32:11 +0300

    fix linter

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -17,14 +17,14 @@ export default function calculate(obj, buttonName) {
     if (buttonName === '0' && obj.next === '0') {
       return {};
     }
-    // If there is an operation, update next
+    
     if (obj.operation) {
       if (obj.next && obj.next !== '0') {
         return { ...obj, next: obj.next + buttonName };
       }
       return { ...obj, next: buttonName };
     }
-    // If there is no operation, update next and clear the value
+    
     if (obj.next && obj.next !== '0') {
       return {
         next: obj.next + buttonName,
@@ -64,7 +64,7 @@ export default function calculate(obj, buttonName) {
         operation: null,
       };
     }
-    // '=' with no operation, nothing to do
+    
     return {};
   }
 
@@ -78,20 +78,12 @@ export default function calculate(obj, buttonName) {
     return {};
   }
 
-  // Button must be an operation
 
-  // When the user presses an operation button without having entered
-  // a number first, do nothing.
-  // if (!obj.next && !obj.total) {
-  //   return {};
-  // }
-
-  // User pressed an operation after pressing '='
   if (!obj.next && obj.total && !obj.operation) {
     return { ...obj, operation: buttonName };
   }
 
-  // User pressed an operation button and there is an existing operation
+  
   if (obj.operation) {
     if (obj.total && !obj.next) {
       return { ...obj, operation: buttonName };
@@ -108,14 +100,12 @@ export default function calculate(obj, buttonName) {
     };
   }
 
-  // no operation yet, but the user typed one
 
-  // The user hasn't typed a number yet, just save the operation
   if (!obj.next) {
     return { operation: buttonName };
   }
 
-  // save the operation and shift 'next' into 'total'
+ 
   return {
     total: obj.next,
     next: null,
