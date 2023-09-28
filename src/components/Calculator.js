3975687883f44e7c08e5f69686487adf01dbfe66
Naol Kidanu <sabelkidanu18@gmail.com>
2023-09-28 17:10:53 +0300

    add some styles

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,10 +2,12 @@ import React from "react";
 import "../style.css";
 import CalcBtns from "./CalcBtns";
 import Quote from "./Quotes";
+import Home from "./Home";
 
 const Calculator = () => (
   <>
     <div className="calc__grid">
+      <Home />
       <CalcBtns />
       <Quote />
     </div>
