06fc29e5a6bab93703b2c94ff8f43ae7395fa916
Naol Kidanu <sabelkidanu18@gmail.com>
2023-09-28 18:08:39 +0300

    create route and import on App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import './style.css';
-import Calculator from './components/Calculator';
+import React from "react";
+import "./style.css";
+import { Route, Routes, Outlet, Link } from "react-router-dom";
+import Calculator from "./components/Calculator";
+import DisplayQuote from "./components/Quotes";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <Calculator />
+      <div className="wrapper">
+        <h1>Math Magician</h1>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="calculator" element={<Calculator />} />
+            <Route path="quotes" element={<DisplayQuote />} />
+          </Route>
+        </Routes>
+      </div>
     </>
   );
 }
